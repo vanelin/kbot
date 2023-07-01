4015d6b4f77310ebc095ce051a216cca8e347e3e
ivan.voloboyev <vano3231@gmail.com>
2023-07-01 18:38:21 +0300

    Add monitoring

--- cmd/kbot.go
+++ cmd/kbot.go
@@ -69,20 +69,6 @@ to quickly create a Cobra application.`,
 	},
 }
 
-type CryptoData struct {
-	Id                string `json:"id"`
-	Rank              string `json:"rank"`
-	Symbol            string `json:"symbol"`
-	Name              string `json:"name"`
-	Supply            string `json:"supply"`
-	MaxSupply         string `json:"maxSupply"`
-	MarketCapUsd      string `json:"marketCapUsd"`
-	VolumeUsd24Hr     string `json:"volumeUsd24Hr"`
-	PriceUsd          string `json:"priceUsd"`
-	ChangePercent24Hr string `json:"changePercent24Hr"`
-	Vwap24Hr          string `json:"vwap24Hr"`
-}
-
 type MarketResponse struct {
 	Data []MarketData `json:"data"`
 }
